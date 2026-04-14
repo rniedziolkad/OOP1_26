8c03cf1e7aade3340b428dc8009e782b5bd3fa37
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-04-14 16:52:42 +0200

    todo

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: Zadanie 6 "Pliki, wyjątki"
         List<Person> loaded = Person.fromCsv("family.csv");
         Person.toBinaryFile("binary.data", loaded);
         System.out.println("Loaded list");
