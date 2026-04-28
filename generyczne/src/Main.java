d7c0e8b2d84fe441bc6c7642b071a359ebf64ae1
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-04-28 15:50:57 +0200

    main

--- generyczne/src/Main.java
+++ generyczne/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        CustomList customList = new CustomList();
+        customList.addLast(0);
+        customList.addLast(1);
+        customList.addLast(5);
+        customList.addLast(-3);
+        customList.addFirst(100);
+        customList.addLast(33);
 
+        System.out.println(customList.getFirst());
+        System.out.println(customList.getLast());
     }
 }
