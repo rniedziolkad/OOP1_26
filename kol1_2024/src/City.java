edba4e09a9cce2a12fc8b48286660a264b70540d
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-05 16:23:55 +0200

    city

---
+++ kol1_2024/src/City.java
@@ -0,0 +1,48 @@
+import org.w3c.dom.xpath.XPathResult;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class City {
+    private String city_name;
+    private int clock_region;
+    private String longitude;
+    private String latitude;
+
+    public City(String city_name, int clock_region, String longitude, String latitude) {
+        this.city_name = city_name;
+        this.clock_region = clock_region;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public static Map<String, City> parseFile (String file_name) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(file_name));
+        Map<String, City> result = new HashMap<>();
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            City parseLine = parseLine(line);
+            result.put(parseLine.city_name, parseLine);
+        }
+        return result;
+    }
+
+    private static City parseLine (String file_line) {
+        String[] parts = file_line.split(",");
+        return new City(parts[0], Integer.parseInt(parts[1]), parts[2], parts[3]);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "city_name='" + city_name + '\'' +
+                ", clock_region=" + clock_region +
+                ", longitude='" + longitude + '\'' +
+                ", latitude='" + latitude + '\'' +
+                '}';
+    }
+}
--- kol1_2024/src/Main.java
+++ kol1_2024/src/Main.java
@@ -1,8 +1,12 @@
+import java.io.FileNotFoundException;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         DigitalClock digitalClock= new DigitalClock(DigitalClock.Type.H12);
         digitalClock.setTime(23, 59, 59);
         System.out.println("Godzina: " + digitalClock.toString());
+        Map<String, City> v = City.parseFile("strefy.csv");
+        System.out.println(v);
     }
-
 }
