c9d5e82443a38ece6fd5fb94ab500362c2fcbaef
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-19 15:59:32 +0200

    wykonywanie poleceń sql

--- site/src/main/java/pl/umcs/oop/Main.java
+++ site/src/main/java/pl/umcs/oop/Main.java
@@ -3,6 +3,7 @@ package pl.umcs.oop;
 
 import pl.umcs.oop.database.DatabaseConnection;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,6 +12,7 @@ public class Main {
         DatabaseConnection.connect("test.db");
         // Statement - obiekt używany do wykonywania "statycznego" polecenia SQL
         Statement stmt = DatabaseConnection.getConnection().createStatement();
+
         stmt.execute("""
            CREATE TABLE IF NOT EXISTS account (
            id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,6 +20,21 @@ public class Main {
            password TEXT
            )""");
 
+        stmt.execute("""
+            INSERT OR IGNORE INTO account (username, password)
+            VALUES ("user1", "pass1"),
+                   ("user2", "pass2"),
+                   ("user3", "pass3");
+        """);
+
+        // ResultSet - przechwuje wiersze z wyniku zapytania SQL
+        ResultSet rs = stmt.executeQuery("""
+            SELECT * FROM account;
+        """);
+
+        while (rs.next()) {
+            System.out.println(rs.getString("username") + ": " + rs.getString("password"));
+        }
 
 
     }
