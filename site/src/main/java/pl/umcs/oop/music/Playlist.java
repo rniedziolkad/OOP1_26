c3dbfb95e657580b132f79fc6697d4c7833c776f
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-26 15:57:17 +0200

    Metoda atSecond

--- site/src/main/java/pl/umcs/oop/music/Playlist.java
+++ site/src/main/java/pl/umcs/oop/music/Playlist.java
@@ -1,7 +1,18 @@
 package pl.umcs.oop.music;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Playlist extends ArrayList<Song> {
-    public Song
+    public Song atSecond (int sec) {
+        int elapsed = 0;
+        for(int i = 0; i < this.size(); i++) {
+            Song song = this.get(i);
+            if (sec >= elapsed || sec < elapsed + song.duration()) {
+                return song;
+            }
+            elapsed += song.duration();
+        }
+        return null;
+    }
 }
