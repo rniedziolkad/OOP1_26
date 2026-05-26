4419acddc82adc3e7f62947fadc10367db1cb42a
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-26 16:33:22 +0200

    BeforeAll oraz AfterAll

--- site/src/test/java/pl/umcs/oop/music/SongTest.java
+++ site/src/test/java/pl/umcs/oop/music/SongTest.java
@@ -1,6 +1,8 @@
 package pl.umcs.oop.music;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pl.umcs.oop.database.DatabaseConnection;
 
@@ -8,13 +10,22 @@ import java.util.Optional;
 
 public class SongTest {
 
+    // przed wszystkimi testami (raz)
+    @BeforeAll
+    static void connectToDatabase() {
+        DatabaseConnection.connect("songs.db");
+    }
+
     @Test
     void checkReadCorrectIndex() {
-        DatabaseConnection.connect("songs.db");
-
         Optional<Song> s1 = Song.Persistence.read(1);
 
         Assertions.assertTrue(s1.isPresent());
         Assertions.assertEquals(new Song("The Beatles", "Hey Jude", 431), s1.get());
+    }
+
+    @AfterAll
+    static void disconnectFromDatabase() {
+        DatabaseConnection.disconnect();
     }
 }
