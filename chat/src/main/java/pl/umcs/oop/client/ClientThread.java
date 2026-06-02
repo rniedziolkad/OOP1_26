cf7b5db175f1c90299d0a5a238496bbbfd0bae6f
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-06-02 16:26:59 +0200

    broadcast

--- chat/src/main/java/pl/umcs/oop/client/ClientThread.java
+++ chat/src/main/java/pl/umcs/oop/client/ClientThread.java
@@ -9,22 +9,33 @@ import java.util.Scanner;
 public class ClientThread extends Thread {
     private Socket socket; // gniazdo, na którym połączony jest klient
     private List<ClientThread> allClients; // każdy klient ma referencję do listy wszystkich klientów
+    PrintWriter writer;
     public ClientThread(Socket socket, List<ClientThread> allClients) {
         this.socket = socket;
         this.allClients = allClients;
+    }
+
+    public void broadcast(String message) {
+        for (ClientThread ct : allClients) {
+            ct.send(message);
+        }
+    }
+
+    public void send(String message) {
+        writer.println(message);
     }
 
     @Override
     public void run() {
         try {
             Scanner scanner = new Scanner(socket.getInputStream()); // do odczytywania wiadomosci od klienta
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true); // do wysyłania wiadomości do klienta
+            writer = new PrintWriter(socket.getOutputStream(), true); // do wysyłania wiadomości do klienta
             String message;
             while (scanner.hasNextLine()) {
                 // czytamy wiadomości w nieskończoność (dopóki jest połączenie)
                 message = scanner.nextLine();
                 System.out.println("Otrzymano wiadomość: " + message);
-                writer.println("echo: " + message);
+                broadcast(message);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
