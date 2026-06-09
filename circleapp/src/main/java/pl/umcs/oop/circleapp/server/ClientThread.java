cb9c03273264a996bfc194cd70e352c1ea3c7206
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-06-09 18:28:36 +0200

    serwer broadcastujący

---
+++ circleapp/src/main/java/pl/umcs/oop/circleapp/server/ClientThread.java
@@ -0,0 +1,49 @@
+package pl.umcs.oop.circleapp.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThread implements Runnable {
+    private final Socket socket;
+    private final BufferedReader reader;
+    private final PrintWriter writer;
+    private final Server server;
+
+    public ClientThread(Socket socket, Server server) throws IOException {
+        this.socket = socket;
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(socket.getOutputStream(), true);
+        this.server = server;
+    }
+
+    public void send(String message) {
+        writer.println(message);
+    }
+
+    private void close() throws IOException {
+        socket.close();
+        server.removeHandler(this);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Client connected: " + socket);
+        String message;
+        try {
+            while ((message = reader.readLine()) != null)
+                server.broadcast(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                close();
+                System.out.println("Client disconnected");
+            } catch (IOException e) {
+                System.err.println("Error disconnecting");
+            }
+        }
+    }
+}
---
+++ circleapp/src/main/java/pl/umcs/oop/circleapp/server/Server.java
@@ -0,0 +1,52 @@
+package pl.umcs.oop.circleapp.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private CopyOnWriteArrayList<ClientThread> handlers = new CopyOnWriteArrayList<>();
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void disconnectHandlers() {
+        handlers.forEach(handler -> handler.send("disconnected"));
+        handlers.clear();
+    }
+
+    public void removeHandler(ClientThread handler) {
+        handlers.remove(handler);
+    }
+
+    public void broadcast(String message) {
+        for (ClientThread ct : handlers) {
+            ct.send(message);
+        }
+    }
+
+    public void listen() throws IOException {
+        System.out.println("Server started");
+        while (true) {
+            System.out.println("Waiting for connection...");
+            Socket socket = serverSocket.accept();
+            ClientThread ct = new ClientThread(socket, this);
+            Thread thread = new Thread(ct);
+            thread.start();
+            handlers.add(ct);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(5000);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.disconnectHandlers();
+        }));
+
+        server.listen();
+    }
+}
